d694896ecb0e7bf8c7a4fb0d45f22980d6866cd7
idoreg21 <idoreg312@gmail.com>
2025-07-04 15:30:37 +0000

    almost done

--- log.c
+++ log.c
@@ -23,13 +23,12 @@ struct Server_Log {
 
 // Creates a new server log instance (stub)
 server_log create_log() {
-
+    int rc;
     server_log log = malloc(sizeof(struct Server_Log));
     if(!log){
         perror("Couldn't malloc a log struct");
         return NULL;
     }
-
     log -> log_size = 0;
     log -> log_capacity = MAXBUF;
     log -> log_buffer = malloc(log -> log_capacity);     // Initial size for a buffer
@@ -43,73 +42,115 @@ server_log create_log() {
     log -> readers_inside = 0;
     log -> writers_inside = 0;
     log -> writers_waiting = 0;
-    pthread_mutex_init(&log -> log_lock, NULL);
-    pthread_cond_init(&log -> read_allowed, NULL);
-    pthread_cond_init(&log -> write_allowed, NULL);
+    if((rc = pthread_mutex_init(&log -> log_lock, NULL)) != 0){
+        posix_error(rc,"pthread_mutex_init Error");
+    }
+    if((rc = pthread_cond_init(&log -> read_allowed, NULL)) != 0){
+        posix_error(rc,"pthread_cond_init Error");
+    }
+    if((rc = pthread_cond_init(&log -> write_allowed, NULL)) != 0){
+        posix_error(rc,"pthread_cond_init Error");
+    }
 
     return log;
-    // TODO: Allocate and initialize internal log structure
 }
 
 // Destroys and frees the log (stub)
 void destroy_log(server_log log) {
+    int rc;
     if (!log) return;
     free(log->log_buffer);
-    pthread_mutex_destroy(&log->log_lock);
-    pthread_cond_destroy(&log->read_allowed);
-    pthread_cond_destroy(&log->write_allowed);
+    if((rc = pthread_mutex_destroy(&log->log_lock)) != 0){
+        posix_error(rc,"pthread_mutex_destroy Error");
+
+    }
+    if((rc = pthread_cond_destroy(&log->read_allowed)) != 0){
+        posix_error(rc,"pthread_cond_destroy Error");
+
+    }
+    if((rc =pthread_cond_destroy(&log->write_allowed)) != 0){
+        posix_error(rc,"pthread_cond_destroy Error");    
+    }
     free(log);
 }
 
 // Returns dummy log content as string (stub)
 int get_log(server_log log, char** dst) {
-    // TODO: Return the full contents of the log as a dynamically allocated string
     // This function should handle concurrent access
-
-    pthread_mutex_lock(&(log->log_lock));
-    while((log->writers_inside) + (log->writers_waiting) > 0) {
-        pthread_cond_wait(&(log->read_allowed), &(log->log_lock));
+    int rc;
+    if((rc = pthread_mutex_lock(&(log->log_lock))) != 0){
+        posix_error(rc,"pthread_mutex_lock Error");
+    }
+    while((log->writers_inside) > 0 || (log->writers_waiting) > 0){
+        if((rc = pthread_cond_wait(&(log->read_allowed), &(log->log_lock))) != 0){
+            posix_error(rc,"pthread_cond_wait Error");
+        }
     }
     (log->readers_inside)++;
-    pthread_mutex_unlock(&(log->log_lock));
+    if((rc =pthread_mutex_unlock(&(log->log_lock))) != 0){
+        posix_error(rc,"pthread_mutex_unlock Error");
+    }
 
-    *dst = malloc(log->log_size + 1);
+    size_t len = log->log_size;
+    if(len > 0 && log->log_buffer[len - 1] == '\n'){
+        len--;
+    }
+
+    *dst = malloc(len + 1);
     if(*dst == NULL){                                       // Handle with a failed reading
         perror("Couldn't malloc get_log");
-        pthread_mutex_lock(&(log->log_lock));
+        if((rc = pthread_mutex_lock(&(log->log_lock))) != 0){
+            posix_error(rc,"pthread_mutex_lock Error");
+        }
         (log->readers_inside)--;
         if((log->readers_inside) == 0){
-            pthread_cond_signal(&(log->write_allowed));
+            if((rc = pthread_cond_signal(&(log->write_allowed))) != 0){
+                posix_error(rc,"pthread_cond_signal Error");
+            }
         }
-        pthread_mutex_unlock(&(log->log_lock));
+        if((rc = pthread_mutex_unlock(&(log->log_lock))) != 0){
+            posix_error(rc,"pthread_mutex_unlock Error");
+        }
         return 0;
     }
 
-    memcpy(*dst, log->log_buffer, log->log_size);
-    (*dst)[log->log_size] = '\0';
+    memcpy(*dst, log->log_buffer, len);
+    (*dst)[len] = '\0';
 
-    pthread_mutex_lock(&(log->log_lock));
+    if((rc = pthread_mutex_lock(&(log->log_lock))) != 0){
+        posix_error(rc,"pthread_mutex_lock Error");
+    }
     (log->readers_inside)--;
     if((log->readers_inside) == 0){
-        pthread_cond_signal(&(log->write_allowed));
+        if((rc = pthread_cond_signal(&(log->write_allowed))) != 0){
+            posix_error(rc,"pthread_cond_signal Error");
+        }
     }
-    pthread_mutex_unlock(&(log->log_lock));
-
-    return (int)log->log_size;
+    if((rc = pthread_mutex_unlock(&(log->log_lock))) != 0){
+        posix_error(rc,"pthread_mutex_unlock Error");
+    }
+    return (int)len;
 }
 
 // Appends a new entry to the log (no-op stub)
 void add_to_log(server_log log, const char* data, int data_len) {
-
-(log->writers_waiting)++;
-    pthread_mutex_lock(&log->log_lock);
-    while(log->writers_inside > 0 || log->readers_inside > 0) {
-        pthread_cond_wait(&log->write_allowed, &log->log_lock);
+    int rc;
+    if((rc = pthread_mutex_lock(&log->log_lock)) != 0){
+        posix_error(rc,"pthread_mutex_lock Error");
     }
-    (log->writers_inside)++;
+    (log->writers_waiting)++;
+    while((log->writers_inside + log->readers_inside) > 0) {
+        if((rc = pthread_cond_wait(&log->write_allowed, &log->log_lock)) != 0){
+                posix_error(rc,"pthread_cond_wait Error");
+        }
+    }
     (log->writers_waiting)--;
+    (log->writers_inside)++;
     usleep(200000);    
-    pthread_mutex_unlock(&log->log_lock);
+
+    if((rc = pthread_mutex_unlock(&(log->log_lock))) != 0){
+        posix_error(rc,"pthread_mutex_unlock Error");
+    }
 
     if (log->log_size + data_len + 1 > log->log_capacity) { // Case we need to increase the log
         size_t new_log_capacity = (log->log_capacity)*2;
@@ -135,15 +176,26 @@ void add_to_log(server_log log, const char* data, int data_len) {
         log->log_buffer[log->log_size] = '\0';              // End the new log    
     }
 
-    pthread_mutex_lock(&log->log_lock);
-    (log->writers_inside)--;
-    if((log->writers_inside) == 0) {
-	if(log->writers_waiting > 0){
-		 pthread_cond_signal(&(log->write_allowed));
-	}
-	else{
-		pthread_cond_broadcast(&(log->read_allowed));
-	}
+    if((rc = pthread_mutex_lock(&log->log_lock)) != 0){
+        posix_error(rc,"pthread_mutex_lock Error");
     }
-    pthread_mutex_unlock(&(log->log_lock));
+
+    (log->writers_inside)--;
+    if(log->writers_inside == 0){
+        if(log->writers_waiting > 0){
+		    if((rc = pthread_cond_signal(&(log->write_allowed))) != 0){
+                posix_error(rc,"pthread_cond_signal Error");
+            }
+        }
+        else{
+            if((rc = pthread_cond_broadcast(&(log->read_allowed))) != 0){
+                posix_error(rc,"pthread_cond_broadcast Error");
+            }
+        }
+	    
+    }
+    if((rc = pthread_mutex_unlock(&(log->log_lock))) != 0){
+        posix_error(rc,"pthread_mutex_unlock Error");
+    }
 }
+
--- server.c
+++ server.c
@@ -41,7 +41,7 @@ pthread_cond_t queue_not_full = PTHREAD_COND_INITIALIZER;
 // Parses command-line arguments
 void getargs(int *port, int *threads, int *queue_size, int argc, char *argv[])
 {
-    if (argc < 4) {
+    if (argc != 4) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         exit(1);
     }
@@ -55,11 +55,15 @@ void getargs(int *port, int *threads, int *queue_size, int argc, char *argv[])
 // that process requests from a synchronized queue.
 
 void enqueue(int connfd, struct timeval arrival_time) {
+    int rc;
+    if((rc = pthread_mutex_lock(&queue_lock)) != 0){
+        posix_error(rc,"pthread_mutex_lock Error");
+    }
 
-    pthread_mutex_lock(&queue_lock);
-
-    while ((queued_threads + active_threads) == MAX_QUEUE_SIZE) {
-        pthread_cond_wait(&queue_not_full, &queue_lock);            // BLOCK until space
+    while ((queued_threads + active_threads) >= MAX_QUEUE_SIZE) {
+        if((rc =pthread_cond_wait(&queue_not_full, &queue_lock)) != 0){
+            posix_error(rc,"pthread_cond_wait Error");
+        }          
     }
 
     connection_queue[tail].connfd = connfd;
@@ -67,13 +71,17 @@ void enqueue(int connfd, struct timeval arrival_time) {
     tail = (tail + 1) % MAX_QUEUE_SIZE;
     queued_threads++;
 
-    pthread_cond_signal(&queue_not_empty);                          // wake a worker
-    pthread_mutex_unlock(&queue_lock);
+    if((rc =pthread_cond_signal(&queue_not_empty)) != 0){
+        posix_error(rc,"pthread_cond_signal Error");
+    }
+    if((rc = pthread_mutex_unlock(&queue_lock)) != 0){
+        posix_error(rc,"pthread_mutex_unlock Error");
+    }
 
 }
 
 void* worker_thread(void* arg){
-
+    int rc;
     thread_arg* t_arg = (thread_arg*)arg;
     server_log log = t_arg->log;
     int thread_id = t_arg->thread_id;
@@ -90,18 +98,23 @@ void* worker_thread(void* arg){
 
     while(1){
 
-        pthread_mutex_lock(&queue_lock);                            // Start Atomic process, Dont touch the queue until we update its parmeters
-        
-        while(queued_threads == 0){                                 // If the queue is Empty, Workers on hold
-            pthread_cond_wait(&queue_not_empty, &queue_lock);       // If queue is empty and queue_not_empty, while queue_lock, continue from here
+        if((rc = pthread_mutex_lock(&queue_lock)) != 0){                        // Start Atomic process, Dont touch the queue until we update its parmeters
+            posix_error(rc,"pthread_mutex_lock Error");
         }
-
+        while(queued_threads == 0){                                 // If the queue is Empty, Workers on hold
+            if((rc =pthread_cond_wait(&queue_not_empty, &queue_lock)) != 0){      // If queue is empty and queue_not_empty, while queue_lock, continue from here
+                posix_error(rc,"pthread_cond_wait Error");
+            }
+        }
+        
         current_connection_fd = connection_queue[head];             // Get the next (FIFO) connection to handle
         head = (head+1) % MAX_QUEUE_SIZE;
         queued_threads--;                                           // Consider as unqueued, will be handled
         active_threads++;                                           // It is no longer queued, consider as active
 
-        pthread_mutex_unlock(&queue_lock);
+        if((rc = pthread_mutex_unlock(&queue_lock)) != 0){
+            posix_error(rc,"pthread_mutex_unlock Error");
+        }
 
 	struct timeval now, dispatch;
 	long sec_tmp, usec_tmp;
@@ -118,13 +131,19 @@ void* worker_thread(void* arg){
 
 	dispatch.tv_sec = sec_tmp;
 	dispatch.tv_usec = usec_tmp;
-        requestHandle(current_connection_fd.connfd, current_connection_fd.arrival_time, dispatch, t, log);        // TODO check if this seperation causes context-switch jamming
+        requestHandle(current_connection_fd.connfd, current_connection_fd.arrival_time, dispatch, t, log);        
         Close(current_connection_fd.connfd);
 
-        pthread_mutex_lock(&queue_lock);
+        if((rc =pthread_mutex_lock(&queue_lock)) != 0){
+            posix_error(rc,"pthread_mutex_lock Error");
+        }
         active_threads--;
-        pthread_cond_signal(&queue_not_full);
-        pthread_mutex_unlock(&queue_lock);
+        if((rc = pthread_cond_signal(&queue_not_full)) != 0){
+            posix_error(rc,"pthread_cond_signal Error");
+        }
+        if((rc = pthread_mutex_unlock(&queue_lock)) != 0){
+            posix_error(rc,"pthread_mutex_unlock Error");
+        }
         
     }
     free(t);                                                        // Memory cleanup
@@ -133,6 +152,7 @@ void* worker_thread(void* arg){
 
 int main(int argc, char *argv[])
 {
+    int rc;
     server_log log = create_log();                                  // Create the global server log
 
     int listenfd, connfd, port, clientlen;
@@ -152,19 +172,33 @@ int main(int argc, char *argv[])
         args[i]->thread_id = i + 1;                                 // IDs from 1 to N
         args[i]->log = log;
                                                                     // Send each thread to worker_thread
-        pthread_create(&threads[i], NULL, worker_thread, (void*)args[i]);
+        if((rc = pthread_create(&threads[i], NULL, worker_thread, (void*)args[i])) != 0){
+            posix_error(rc,"pthread_create Error");
+        }
     }
-	struct timeval arrival;                           // Time shit TODO
     
     while (1) {
-        
+        if((rc =pthread_mutex_lock(&queue_lock)) != 0){
+            posix_error(rc,"pthread_mutex_lock Error");
+        }
+        while((queued_threads+active_threads)>=MAX_QUEUE_SIZE){
+            if((rc =pthread_cond_wait(&queue_not_full,&queue_lock)) != 0){
+                posix_error(rc,"pthread_cond_wait Error");
+            }
+        }
+        if((rc =pthread_mutex_unlock(&queue_lock)) != 0){
+            posix_error(rc,"pthread_mutex_unlock Error");
+        }
+
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *) &clientlen);
+
+        struct timeval arrival;                           
 	    gettimeofday(&arrival, NULL);
         enqueue(connfd, arrival);
     }
 
     destroy_log(log);                                               // Clean up the server log before exiting
 
-    // TODO: HW3 — Add cleanup code for thread pool and queue verify it is enough
+    
 }
